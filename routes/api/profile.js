eb2ed0da6116d3934fd7c56509e33054829c54d1
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2021-07-01 09:59:54 -0400

    added axios, registering new users from client

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,26 @@
 const express = require('express');
-
+const auth = require('../../middleware/auth')
 const router = express.Router()
+const Profile = require ('../../models/Profile')
+const User = require('../../models/User')
+
+// GET api/profile/self
+// get current user profile
+router.get('/self', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', avatar])
+        if (!profile) {
+            return res.status(400).json({msg: "There is no profile for this user"})
+        }
+        res.json(profile)
+        
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server errorß')
+    }
+})
 
 
-// GET api/profile
-router.get('/', (req, res) => res.send('Profile Route'))
+
 
 module.exports = router;
